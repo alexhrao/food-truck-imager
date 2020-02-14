f48d8ba895201cfbe542954757a9926e536fd033
Alexander Rao <alexhrao@gmail.com>
2020-02-14 11:35:45 -0500

    Add better shell starter

--- runner.sh
+++ runner.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 # make sure we have latest
-git pull
-
+if [ $# -lt 1 ]; then
+    >&2 echo "Usage: ./runner.sh <location>"
+    exit 1
+fi
 source ./venv/bin/activate
 source ./setup.sh
-python3 main.py $1 &
+python3 main.py $1 > /dev/null &
 cam_pid=$!
 
+while ! lsof -i:10000 > /dev/null;
+do
+    sleep 1
+done
+trap "curl localhost:10000 --silent" EXIT
+echo "Listening for changes..."
 while :
 do
-    if ! git diff-index --quiet HEAD --; then
-        curl -XGET localhost:10000 --quiet
+    if ! git diff-index --quiet origin/master --; then
+        curl -XGET localhost:10000 --silent
         echo "New changes detected - installing"
         wait $cam_pid
         git pull
-        python3 main.py $1 &
+        python3 main.py $1 > /dev/null &
         cam_pid=$!
+        while ! lsof -i:10000;
+        do
+            sleep 1
+        done
         echo "Successfully restarted"
     fi
-done
